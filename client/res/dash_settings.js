a449247ef1ec09f78e717f1e16dc5b568e3d7224
Drake Perrior-Small <perrid3@rpi.edu>
2016-10-31 17:38:30 -0400

    add placeholders for js listeners, make data sources mutable

--- client/res/dash_settings.js
+++ client/res/dash_settings.js
@@ -1,3 +1,49 @@
+function addSource(){
+    sources = document.getElementById("sources");
+    var selector = document.createElement("select");
+    selector.classList = ["source"];
+    source_count = source_array.length;
+
+    for (var i = 0; i < source_count; i += 1) {
+        option = document.createElement('option');
+        option.setAttribute('value', source_array[i].value);
+        option.appendChild(document.createTextNode(source_array[i].text));
+        selector.appendChild(option);
+    }
+    sources.appendChild(selector);
+}
+
+var source_array = [
+    {value: "404", text: "404"},
+    {value: "d404", text: "404 rate"},
+    {value: "lod", text: "Page Loads"},
+    {value: "dlod", text: "Page Load Rate"},
+    {value: "lat", text: "Latency"},
+    {value: "usr0", text: "API 1"},
+    {value: "usr1", text: "API 2"},
+    {value: "usr2", text: "API 3"}
+]
+
+document.getElementById("plus").addEventListener('click', function() {
+    alert("foo");
+}, false);
+
+document.getElementById("minus").addEventListener('click', function() {
+    alert("bar");
+}, false);
+
+document.getElementById("gear").addEventListener('click', function() {
+    alert("baz");
+}, false);
+
+document.getElementById("more").addEventListener('click', function() {
+    alert("I should probably remove all of this duplicate code.");
+}, false);
+
 var greeting = document.getElementById("name");
 var name = "User"; //we can probably get the name from th
 (new Date().getHours() > 12) ? greeting.innerHTML = "Good Afternoon, "+name+"!" : greeting.innerHTML = "Good Morning, "+name+"!";
+
+for (var i = 0; i < source_array.length; i += 1) { 
+    addSource(); document.getElementById("sources").lastChild.selectedIndex = i; 
+}
