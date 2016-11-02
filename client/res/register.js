c3098c3dcfbfd72ca9fa51a57988a37a115e432e
Drake Perrior-Small <drake@azwide.com>
2016-11-02 12:00:16 -0400

    moved JS to source file

--- client/res/register.js
+++ client/res/register.js
@@ -2,31 +2,47 @@ var flag_register = 0;
 var registration = document.getElementById("register");
 var login = document.getElementById("login");
 
-var reg_info = [document.getElementsByName("name")[0],document.getElementsByName("email")[0]];
+var reg_info = document.getElementsByName("name")[0]
 console.log(reg_info);
 
 function register() {
     flag_register = 1;
-        for (i = 0; i < reg_info.length; i++) {
-            reg_info[i].className = "in";
-            reg_info[i].required = true;
-            registration.innerHTML = "Just Kidding";
-						//document.getElementById("main").style.height = "25vh";
-            login.innerHTML = "Register";
-        }
+    reg_info.className = "in";
+    reg_info.required = true;
+    registration.innerHTML = "Just Kidding";
+    login.innerHTML = "Register";
 }
 
 function unregister() {
     flag_register = 0;
-        for (i = 0; i < reg_info.length; i++) {
-            reg_info[i].className = "in hide";
-            reg_info[i].required = false;
-						//document.getElementById("main").style.height = "20vh";
-            registration.innerHTML = "Register";
-            login.innerHTML = "Login";
-        }
+    reg_info.className = "in hide";
+    reg_info.required = false;
+    registration.innerHTML = "Register";
+    login.innerHTML = "Login";
 }
 
+function sendForm(){
+    formData = $("#auth").serializeObject();
+    if (document.getElementById("login").innerHTML == "Login"){
+        $.ajax({
+            url: '/api/login',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(formData),
+            dataType: 'json',
+            success:function(e){}
+        });
+    } else {
+        $.ajax({
+            url: '/api/signup',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(formData),
+            dataType: 'json',
+            success:function(e){}
+        });
+    }
+}
 
 registration.addEventListener('click', function() {
     flag_register == 0 ? register(): unregister() ;
