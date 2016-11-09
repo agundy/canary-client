8fa34cd11b2d0bf8e1787628d8a15c4accc94b63
Drake Perrior-Small <drake@azwide.com>
2016-11-08 19:42:02 -0500

    hopefull last

--- client/res/login/login.controller.js
+++ client/res/login/login.controller.js
@@ -22,7 +22,7 @@ app.controller('LoginCtrl', function($scope, $location, Auth) {
 
 //gonna test including this in this js file, will cleanup to standards after
 
-window.onload=function(){ 
+$("#register").load($(this).attr("href"),function(){
     var flag_register = 0;
     var registration = document.getElementById("register");
     var login = document.getElementById("login");
