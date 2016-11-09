46e3a43e0fcfd42eb26b0f1eb0dee21b78f0fe4f
Drake Perrior-Small <drake@azwide.com>
2016-11-08 19:38:03 -0500

    fourth

--- client/res/login/login.controller.js
+++ client/res/login/login.controller.js
@@ -22,33 +22,35 @@ app.controller('LoginCtrl', function($scope, $location, Auth) {
 
 //gonna test including this in this js file, will cleanup to standards after
 
-var flag_register = 0;
-var registration = document.getElementById("register");
-var login = document.getElementById("login");
-var authform = document.getElementById("auth");
+document.getElementById("register").onload=function(){ 
+    var flag_register = 0;
+    var registration = document.getElementById("register");
+    var login = document.getElementById("login");
+    var authform = document.getElementById("auth");
 
-var reg_info = document.getElementsByName("name")[0];
+    var reg_info = document.getElementsByName("name")[0];
 
-function register() {
-    flag_register = 1;
-    reg_info.className = "in";
-    reg_info.required = true;
-    registration.innerHTML = "Just Kidding";
-    login.innerHTML = "Register";
-    authform.attr("ng-submit","register()");
-}
+    function register() {
+        flag_register = 1;
+        reg_info.className = "in";
+        reg_info.required = true;
+        registration.innerHTML = "Just Kidding";
+        login.innerHTML = "Register";
+        authform.attr("ng-submit","register()");
+    }
 
-function unregister() {
-    flag_register = 0;
-    reg_info.className = "in hide";
-    reg_info.required = false;
-    reg_info.value = "";
-    registration.innerHTML = "Register";
-    login.innerHTML = "Login";
-    authform.attr("ng-submit","login()");
-}
+    function unregister() {
+        flag_register = 0;
+        reg_info.className = "in hide";
+        reg_info.required = false;
+        reg_info.value = "";
+        registration.innerHTML = "Register";
+        login.innerHTML = "Login";
+        authform.attr("ng-submit","login()");
+    }
 
-registration.addEventListener('click', function() {
-    flag_register == 0 ? register(): unregister() ;
-    
-}, false);
+    registration.addEventListener('click', function() {
+        flag_register == 0 ? register(): unregister() ;
+
+    }, false);
+};
