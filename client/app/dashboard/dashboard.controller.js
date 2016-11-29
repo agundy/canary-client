af41678ae80c880c17787d591b24fa5e9ec24156
Barry <midknightsun95@yahoo.com>
2016-11-29 15:21:50 -0500

    HSL colors for angular

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -21,11 +21,31 @@ app.controller('DashboardCtrl', function($scope, $location, Auth, Project, User,
     
     
     $scope.caughtHTTPcodes = {
-        "100" : ["Continue", 0 ], "101" : ["Switching Protocols", 0 ], "102" : ["Processing", 0 ], "200" : ["OK", 0 ], "201" : ["Created", 0 ], "202" : ["Accepted", 0 ], "203" : ["Non-Authoritative Information", 0 ], "204" : ["No Content", 0 ], "205" : ["Reset Content", 0 ], "206" : ["Partial Content", 0 ], "207" : ["Multi-Status", 0 ], "208" : ["Already Reported", 0 ], "226" : ["IM Used", 0 ], "300" : ["Multiple Choices", 0 ], "301" : ["Moved Permanently", 0 ], "302" : ["Found", 0 ], "303" : ["See Other", 0 ], "304" : ["Not Modified", 0 ], "305" : ["Use Proxy", 0 ], "307" : ["Temporary Redirect", 0 ], "308" : ["Permanent Redirect", 0 ], "400" : ["Bad Request", 0 ], "401" : ["Unauthorized", 0 ], "402" : ["Payment Required", 0 ], "403" : ["Forbidden", 0 ], "404" : ["Not Found", 0 ], "405" : ["Method Not Allowed", 0 ], "406" : ["Not Acceptable", 0 ], "407" : ["Proxy Authentication Required", 0 ], "408" : ["Request Timeout", 0 ], "409" : ["Conflict", 0 ], "410" : ["Gone", 0 ], "411" : ["Length Required", 0 ], "412" : ["Precondition Failed", 0 ], "413" : ["Payload Too Large", 0 ], "414" : ["URI Too Long", 0 ], "415" : ["Unsupported Media Type", 0 ], "416" : ["Range Not Satisfiable", 0 ], "417" : ["Expectation Failed", 0 ], "421" : ["Misdirected Request", 0 ], "422" : ["Unprocessable Entity", 0 ], "423" : ["Locked", 0 ], "424" : ["Failed Dependency", 0 ], "426" : ["Upgrade Required", 0 ], "428" : ["Precondition Required", 0 ], "429" : ["Too Many Requests", 0 ], "431" : ["Request Header Fields Too Large", 0 ], "451" : ["Unavailable For Legal Reasons", 0 ], "500" : ["Internal Server Error", 0 ], "501" : ["Not Implemented", 0 ], "502" : ["Bad Gateway", 0 ], "503" : ["Service Unavailable", 0 ], "504" : ["Gateway Timeout", 0 ], "505" : ["HTTP Version Not Supported", 0 ], "506" : ["Variant Also Negotiates", 0 ], "507" : ["Insufficient Storage", 0 ], "508" : ["Loop Detected", 0 ], "510" : ["Not Extended", 0 ], "511" : ["Network Authentication Required", 0]
+        "100" : ["Continue", 0 ], "101" : ["Switching Protocols", 0 ], "102" : ["Processing", 0 ], 
+        "200" : ["OK", 0 ], "201" : ["Created", 0 ], "202" : ["Accepted", 0 ], 
+        "203" : ["Non-Authoritative Information", 0 ], "204" : ["No Content", 0 ], "205" : ["Reset Content", 0 ], 
+        "206" : ["Partial Content", 0 ], "207" : ["Multi-Status", 0 ], "208" : ["Already Reported", 0 ], 
+        "226" : ["IM Used", 0 ], 
+        "300" : ["Multiple Choices", 0 ], "301" : ["Moved Permanently", 0 ], "302" : ["Found", 0 ], 
+        "303" : ["See Other", 0 ], "304" : ["Not Modified", 0 ], "305" : ["Use Proxy", 0 ], 
+        "307" : ["Temporary Redirect", 0 ], "308" : ["Permanent Redirect", 0 ], 
+        "400" : ["Bad Request", 0 ], "401" : ["Unauthorized", 0 ], "402" : ["Payment Required", 0 ], 
+        "403" : ["Forbidden", 0 ], "404" : ["Not Found", 0 ], "405" : ["Method Not Allowed", 0 ], 
+        "406" : ["Not Acceptable", 0 ], "407" : ["Proxy Authentication Required", 0 ], "408" : ["Request Timeout", 0 ], 
+        "409" : ["Conflict", 0 ], "410" : ["Gone", 0 ], "411" : ["Length Required", 0 ], 
+        "412" : ["Precondition Failed", 0 ], "413" : ["Payload Too Large", 0 ], "414" : ["URI Too Long", 0 ], 
+        "415" : ["Unsupported Media Type", 0 ], "416" : ["Range Not Satisfiable", 0 ], "417" : ["Expectation Failed", 0 ], 
+        "421" : ["Misdirected Request", 0 ], "422" : ["Unprocessable Entity", 0 ], "423" : ["Locked", 0 ], 
+        "424" : ["Failed Dependency", 0 ], "426" : ["Upgrade Required", 0 ], "428" : ["Precondition Required", 0 ], 
+        "429" : ["Too Many Requests", 0 ], "431" : ["Request Header Fields Too Large", 0 ], "451" : ["Unavailable For Legal Reasons", 0 ], 
+        "500" : ["Internal Server Error", 0 ], "501" : ["Not Implemented", 0 ], "502" : ["Bad Gateway", 0 ], 
+        "503" : ["Service Unavailable", 0 ], "504" : ["Gateway Timeout", 0 ], "505" : ["HTTP Version Not Supported", 0 ], 
+        "506" : ["Variant Also Negotiates", 0 ], "507" : ["Insufficient Storage", 0 ], "508" : ["Loop Detected", 0 ], 
+        "510" : ["Not Extended", 0 ], "511" : ["Network Authentication Required", 0]
     };
 
-    //Array for Cell colors
-    $scope.colors = [ //(on,off)
+    //Array for Cell colors in hex
+    $scope.colors = [ //(off,on)
 	[ "#d27979" , "#ff4d4d" ],
 	[ "#d2b579" , "#ffc34d" ],
 	[ "#b5d279" , "#c3ff4d" ],
@@ -35,6 +55,15 @@ app.controller('DashboardCtrl', function($scope, $location, Auth, Project, User,
 	[ "#7979d2" , "#4d4dff" ],
 	[ "#b579d2" , "#c44dff" ],
     [ "#d279b5" , "#ff4dc3" ]
+    ];
+    
+    //Array for Cell colors in HSL
+    $scope.colorsHSL = [ //(off,on)
+	[ "hsl(180,100%,75%)" , "hsl(180,100%,50%)" ],
+	[ "hsl(120,100%,75%)" , "hsl(120,100%,75%)" ],
+	[ "hsl(60,100%,75%)" , "hsl(60,100%,50%)" ],
+	[ "hsl(30,100%,75%)" , "hsl(30,100%,50%)" ],
+  [ "hsl(0,100%,75%)" , "hsl(0,100%,50%)" ]
     ];
     
     //Visibility state of project selection menu
@@ -47,10 +76,12 @@ app.controller('DashboardCtrl', function($scope, $location, Auth, Project, User,
         code : ""
     };
     
+    //Gets the color for a data source
     $scope.getColor = function(httpCode){
-        return { 'background-color' : $scope.colors[Math.floor(Number(httpCode) / 100) - 1][$scope.caughtHTTPcodes[httpCode][1]] };
+        return { 'background-color' : $scope.colorsHSL[Math.floor(Number(httpCode) / 100) - 1][$scope.caughtHTTPcodes[httpCode][1]] };
     }
     
+    //Adds data source to selected Project
     $scope.addSource = function() {
         if (angular.isDefined($scope.caughtHTTPcodes[$scope.newDataSource.code])) {
             var newData = {
@@ -64,21 +95,21 @@ app.controller('DashboardCtrl', function($scope, $location, Auth, Project, User,
     
     
 
-    //Pops last data source from selected project
-    //Currently not ported from dash_settings
+    //Pops last data source from selected Project
     $scope.popSource = function(source) {
         var index = $scope.source_array.indexOf(source);
         $scope.source_array.splice(index, 1);
         $scope.squaredSources = angular.copy($scope.doSquaredSources());
     };
 
-    //Currently selected project
+    //Currently selected Project
     $scope.projects = Project.query({}, function(){
         $scope.selectedProject = $scope.projects[0];
     });
+    //Currently logged in User
     $scope.user = User.me();
     
-
+    //Set of values associated with latest Event polled
     $scope.lastEvent = {
             "id":0,
             "host":"",
@@ -94,7 +125,6 @@ app.controller('DashboardCtrl', function($scope, $location, Auth, Project, User,
         name : ""
     };
     
-
     
     //Creates a new token for the selected project
     $scope.newToken = function(project) {
@@ -124,14 +154,16 @@ app.controller('DashboardCtrl', function($scope, $location, Auth, Project, User,
         }
     };
     
-    
+    //Polling refresh rate
     $scope.refreshRate = 1000;
     
+    //Toggle the state of Cells given some data source
     $scope.toggleDataSource = function(someSource) {
         $scope.caughtHTTPcodes[someSource][1] = ($scope.caughtHTTPcodes[someSource][1] + 1) % 2;
         $scope.squaredSources = angular.copy($scope.doSquaredSources());
     };
     
+    //Poll for new Events
     $scope.poll = function(){
         Project.pollEvent({
             id: $scope.selectedProject.id,
